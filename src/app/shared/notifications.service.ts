2ef11f9299ae2dc984f3b4e1849bc187a2bf1a38
Edoardo Pessina <edoardopessina.priva@gmail.com>
2021-02-15 22:56:45 +0100

    Android icons

--- src/app/shared/notifications.service.ts
+++ src/app/shared/notifications.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {OneSignal} from '@ionic-native/onesignal/ngx';
+import {OSNotification, OSNotificationOpenedResult} from '@ionic-native/onesignal';
 
 const oneSignalAppId = '9d182bac-d900-462b-afe1-62523e5aa86e';
 const firebaseSenderId = '52742133080';
@@ -7,7 +8,7 @@ const firebaseSenderId = '52742133080';
 @Injectable({providedIn: 'root'})
 export class NotificationsService {
   private iosSettings = {
-    kOSSettingsKeyAutoPrompt: false,
+    kOSSettingsKeyAutoPrompt: true,
     kOSSettingsKeyInAppLaunchURL: false,
   }
 
@@ -20,13 +21,17 @@ export class NotificationsService {
 
     this.oneSignal.iOSSettings(this.iosSettings)
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
-    this.oneSignal.handleNotificationReceived().subscribe(() => this.handleNotificationReceived());
-    this.oneSignal.handleNotificationOpened().subscribe(() => this.handleNotificationOpened());
+    this.oneSignal.handleNotificationReceived().subscribe(notificationData => this.handleNotificationReceived(notificationData));
+    this.oneSignal.handleNotificationOpened().subscribe(openResult => this.handleNotificationOpened(openResult));
 
     this.oneSignal.endInit();
   }
 
-  private handleNotificationReceived() {}
+  private handleNotificationReceived(notificationData: OSNotification) {
+    console.log('handleNotificationReceived', notificationData)
+  }
 
-  private handleNotificationOpened() {}
+  private handleNotificationOpened(openResult: OSNotificationOpenedResult) {
+    console.log('handleNotificationOpened', openResult)
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
 
-  apiBaseUrl: 'http://localhost:8000',
+  // apiBaseUrl: 'http://localhost:8000',
   // apiBaseUrl: "https://api-simile.como.polimi.it",
-  // apiBaseUrl: 'http://131.175.56.116:8001',
+  apiBaseUrl: 'http://131.175.56.116:8001',
 
   apiVersion: 'v1',
 };
