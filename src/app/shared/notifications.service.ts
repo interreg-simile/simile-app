b212e63cd4a2699f369e28c3bb175736e319bec1
Edoardo Pessina <edoardopessina.priva@gmail.com>
2021-02-20 10:15:31 +0100

    ios notifications

--- src/app/shared/notifications.service.ts
+++ src/app/shared/notifications.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {OneSignal} from '@ionic-native/onesignal/ngx';
-import {OSNotification, OSNotificationOpenedResult} from '@ionic-native/onesignal';
 
 const oneSignalAppId = '9d182bac-d900-462b-afe1-62523e5aa86e';
 const firebaseSenderId = '52742133080';
@@ -21,17 +20,7 @@ export class NotificationsService {
 
     this.oneSignal.iOSSettings(this.iosSettings)
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
-    this.oneSignal.handleNotificationReceived().subscribe(notificationData => this.handleNotificationReceived(notificationData));
-    this.oneSignal.handleNotificationOpened().subscribe(openResult => this.handleNotificationOpened(openResult));
 
     this.oneSignal.endInit();
-  }
-
-  private handleNotificationReceived(notificationData: OSNotification) {
-    console.log('handleNotificationReceived', notificationData)
-  }
-
-  private handleNotificationOpened(openResult: OSNotificationOpenedResult) {
-    console.log('handleNotificationOpened', openResult)
   }
 }
