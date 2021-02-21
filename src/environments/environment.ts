be551d88360443bb45bb19ef07b50bf6dc18a632
Edoardo Pessina <edoardopessina.priva@gmail.com>
2021-02-21 16:23:19 +0100

    Updated version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   apiBaseUrl: 'http://131.175.56.116:8001',
 
   // Prod
-  // apiBaseUrl: "https://api-simile.como.polimi.it",
+  // apiBaseUrl: 'https://api-simile.como.polimi.it',
 
   apiVersion: 'v1',
 };
