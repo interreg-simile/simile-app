431e8db4c410d97629103462fb1bd1553d29f226
epessina <edoardopessina.priv@gmail.com>
2022-11-12 16:34:42 +0100

    Project page

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { ProjectPage } from './project.page';
 import {RouterModule, Routes} from '@angular/router'
 import {TranslateModule} from '@ngx-translate/core'
+import { IonicModule } from '@ionic/angular';
+
+import { ProjectPage } from './project.page';
 
 const routes: Routes = [{path: '', component: ProjectPage}];
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
     TranslateModule
--- src/app/project/project.page.ts
+++ src/app/project/project.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Browser } from '@capacitor/browser';
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.page.html',
   styleUrls: ['./project.page.scss'],
 })
 export class ProjectPage {
+  public projectUrl = 'https://progetti.interreg-italiasvizzera.eu/it/b/78/sistemainformativoperilmonitoraggiointegratodeilaghiinsubriciedeiloroe';
 
-  constructor() {}
+  constructor() { }
 
+  ionViewDidEnter() {
+    const projectLinks = document.querySelectorAll('.project-link');
+    projectLinks.forEach((l) => l.addEventListener('click', () => this.onLinkClick(this.projectUrl)));
+  }
+
+  onLinkClick(url: string) {
+    Browser.open({ url })
+  }
 }
